6048f96c44018bcda3e74fe97e36e0b0b29e0516
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2014-09-15 12:45:27 +0100

    Fix terminal stty configuration

--- vpl_terminal_launcher.sh
+++ vpl_terminal_launcher.sh
@@ -3,8 +3,8 @@ export LC_ALL=$VPL_LANG 1>/dev/null 2>.vpl_set_locale_error
 #if current lang not available use en_US.utf8
 if [ -s .vpl_set_locale_error ] ; then
 	export LC_ALL=en_US.utf8 1>/dev/null 2> /dev/null
-	rm .vpl_set_locale_error 1>/dev/null 2> /dev/null
 fi
-stty sane erase ^H 1>/dev/null 2>/dev/null
+rm .vpl_set_locale_error 1>/dev/null 2> /dev/null
+stty sane iutf8 erase ^? 1>/dev/null 2>/dev/null
 ./vpl_execution
 exit
