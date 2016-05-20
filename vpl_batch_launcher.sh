39adffcb71460015660cf1b6647d95f8ab45427e
Juan Carlos Rodríguez del Pino <jcrodriguez@dis.ulpgc.es>
2016-05-20 18:29:55 +0100

    New launcher for batch tasks

---
+++ vpl_batch_launcher.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+{
+	export LC_ALL=$VPL_LANG 1>/dev/null 2>.vpl_set_locale_error
+	#if current lang not available use en_US.utf8
+	if [ -s .vpl_set_locale_error ] ; then
+		rm .vpl_set_locale_error
+		export LC_ALL=en_US.utf8
+	fi
+	export TERM=dumb
+} &>/dev/null
+./vpl_execution
+exit
