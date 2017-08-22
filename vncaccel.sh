ce9bd9c98a9fb00f3638b340df142cfc61132bff
Juan Carlos Rodríguez del Pino <jcrodriguez@dis.ulpgc.es>
2017-08-22 10:39:18 +0100

    Changes to be based in the use of the LOGLEVEL=8
    Copy model user home directory to /etc/vncaccel

--- vncaccel.sh
+++ vncaccel.sh
@@ -7,43 +7,47 @@
 #               in jail
 
 #Check current configuration
-ls -d $HOME/.??*/
-if [ "$?" == "2" ] ; then
-	echo "I can not generate the VNC accelerate directory due to existence of"
-	echo "local configuration directories. Save it and try again"
-	echo
-	echo "Directories to save:"
-	ls -d $HOME/.??*/
-	exit 0
+function usage {
+	echo "vncaccel.sh 1.0"
+	echo "usage: vncaccel.sh [full path to the model user dir]"
+	echo "-------------------------------------------------------------"
+	echo "To generate the user model"
+	echo "  1) Set configuration DEBUGLEVEL to 8"
+	echo "  2) Run in VPL GUI mode (e.g write a index.html file)"
+	echo "  3) Save the user dir path"
+	echo "  4) Inside VNC run any software that you want to accelerate its load"
+	echo "      e.g.: firefox, a java program, jgrasp, ddd, etc."
+	echo "This script copy the user dir/.dirs to/etc/vncaccel"
+	echo "After running this script you must restart the service"
+}
+if [ "$USER" != "root" ] ; then
+	echo "Error: You must be root"
+    usage
+    exit 1
 fi
-#Prepare VNC launch
+if [ ! -d "$1" ] ; then
+	echo "Error: You must set a valid dir"
+    usage
+    exit 1
+fi
 VNCACCELDIR=/etc/vncaccel
-VPL_LANG=en_US.utf8
-VPL_VNCPASSWD="$RANDOM$RANDOM"
-cp vpl_vnc_launcher.sh $HOME
-cp vpl_vnc_stopper.sh $HOME
-cd $HOME
-touch .vpl_environment.sh
-chmod +x .vpl_environment.sh
-cat > vpl_wexecution << END_OF_SCRIPT
-x-www-browser & >/dev/null
-firefox & >/dev/null
-ddd & > /dev/null
-END_OF_SCRIPT
-./vpl_vnc_launcher &
-echo "VNC launched at port number shown and using password $VPL_VNCPASSWD"
-echo "You can use a vcnclient to configure better the software"
-echo "Waiting 1 minute to start software first time and then will be closed"
-sleep 60
-./vpl_vnc_stopper.sh
-#Copy results
-VNCACCELDIR=/etc/vncaccel
-if [ -d "$VNCACCELDIR" ] ; then
+#Create dir
+if [ -d $VNCACCELDIR ] ;then
 	rm -Rf $VNCACCELDIR
 fi
-mkdir "$VNCACCELDIR"
-cp -a .??* "$VNCACCELDIR"
-rm $VNCACCELDIR/* 2> /dev/null
-chmod -R +r "$VNCACCELDIR"
+mkdir $VNCACCELDIR
+chmod a+xr $VNCACCELDIR
+cp -a $1/.??* $VNCACCELDIR
+#Clean directory
+rm -Rf $VNCACCELDIR/.vnc > /dev/null
+rm $VNCACCELDIR/.??* 2> /dev/null
+#Change access mode
+chown -R nobody.nogroup $VNCACCELDIR/.??* 2>/dev/null
+if [ $? -ne 0 ] ; then
+    chown -R nobody.nobody $VNCACCELDIR/.??* 2>/dev/null
+fi
+find $VNCACCELDIR -type d -print0 | xargs -0 chmod a+xr
+find $VNCACCELDIR -type f -print0 | xargs -0 chmod a+x
+
 
 
