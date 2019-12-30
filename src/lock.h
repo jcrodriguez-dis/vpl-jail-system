e5bb1681195d4976166dbca74d6ca870d41a32ed
jcrodriguez <jcrodriguez@dis.ulpgc.es>
2019-12-30 12:21:57 +0000

    Rewrite Lock class to do not alter the modification date of dirs

--- src/lock.h
+++ src/lock.h
@@ -13,27 +13,50 @@
 #include <sys/types.h>
 #include <syslog.h>
 
+#include "util.h"
+#include "configuration.h"
+
 using namespace std;
 
 class Lock{
 	string filePath;
+	static string getBaseDir() {
+		Configuration* configuration = Configuration::getConfiguration();
+		return configuration->getControlPath() + "/locks";
+	}
+	static string transformDirPath(string &dirPath) {
+		string res = dirPath;
+		for ( size_t i = 0; i < res.size(); i++ ) {
+			if ( res.at(i) == '/' ) {
+				res[i] = '+';
+			}
+		}
+		return res;
+	}
 public:
 	Lock(string DirPath){
-		filePath=DirPath+"/lock";
+		string baseDir = getBaseDir();
+		if ( ! Util::dirExists(baseDir) ) {
+			Util::createDir(baseDir, 0, baseDir.size());
+		}
+		filePath = baseDir + "/" + transformDirPath(DirPath);
 		int ntry=0;
 		int fd;
-		while((fd=open(filePath.c_str(),O_CREAT|O_EXCL|O_WRONLY, 0x600))==-1 && ntry <1000){
+		while ( (fd=open(filePath.c_str(),O_CREAT|O_EXCL|O_WRONLY, 0x600)) == -1
+				&& ntry <1000){
 			struct stat fileInfo;
-			if(stat(filePath.c_str(), &fileInfo)>=0)
-				if(fileInfo.st_mtime+2 < time(NULL)){
+			if(stat(filePath.c_str(), &fileInfo)>=0) {
+				if(fileInfo.st_mtime + 2 < time(NULL)){
 					unlink(filePath.c_str());
 					syslog(LOG_DEBUG,"locking timeout %s",filePath.c_str());
 				}
+			}
 			usleep(5000);
 			ntry++;
 		}
-		if(fd>0)
+		if (fd > 0) {
 			close(fd);
+		}
 
 	}
 	~Lock(){
