971654b8ac048bbf66b3bd352b31a381a3ff9ec2
AitorSDL <aitor.sdl@gmail.com>
2020-09-21 12:27:00 -0400

    Script improved

--- runTest.sh
+++ runTest.sh
@@ -4,10 +4,12 @@ if test -f "src/program-test" ; then
 	./src/program-test
 	if [ "$?" != "0" ] ; then
 		tail /var/log/syslog
-	else
+		exit 1
+	else		
 		echo "All tests passed."
+		exit 0
 	fi
 else 
 	echo "src/program-test not found."
+	exit 2
 fi
-
