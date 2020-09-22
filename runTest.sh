9d6dcd3253f81833e7bfc79102a06e9cb178a0b9
AitorSDL <aitor.sdl@gmail.com>
2020-09-22 12:46:01 -0400

    Added option for C++ 11

--- runTest.sh
+++ runTest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 make all
 if test -f "src/program-test" ; then
+	rm -R cgroup.test 2> /dev/null
+	cp -a cgroup cgroup.test
 	./src/program-test
 	if [ "$?" != "0" ] ; then
 		tail /var/log/syslog
