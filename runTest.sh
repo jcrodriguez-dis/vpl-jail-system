1334025139d4fc88b54349b941fe174558e8841a
jcrodriguez <jcrodriguez@dis.ulpgc.es>
2020-11-07 12:45:18 +0000

    Updates to use the new source location

--- runTest.sh
+++ runTest.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-make all
-if test -f "src/program-test" ; then
+./configure
+make distcheck
+if [ "$?" != "0" ] ; then
+	echo "Error: make distcheck fails."
+	exit 1
+fi
+cd tests
+make program-test
+if test -f program-test ; then
 	rm -R cgroup.test 2> /dev/null
 	cp -a cgroup cgroup.test
-	./src/program-test
+	./program-test
 	if [ "$?" != "0" ] ; then
 		tail /var/log/syslog
 		exit 1
-	else		
+	else
+		rm -R cgroup.test 2> /dev/null
+		rm program-test
 		echo "All tests passed."
 		exit 0
 	fi
 else 
-	echo "src/program-test not found."
+	echo "Error: program-test not found."
 	exit 2
 fi
