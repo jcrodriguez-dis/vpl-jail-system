231315a5b0ef6caf84ad783cbe6933db8bc20db2
jcrodriguez <jcrodriguez@dis.ulpgc.es>
2020-12-02 10:48:32 +0000

    Adds new parameters: REQUEST_MAX_SIZE RESULT_MAX_SIZE
    These parameter control de limits of the request limit size and result
    limit size.

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -11,104 +11,107 @@
 #include "jail_limits.h"
 #include "util.h"
 Configuration *Configuration::singlenton=0;
-string Configuration::generateCleanPATH(string jailPath, string dirtyPATH){
+string Configuration::generateCleanPATH(string path, string dirtyPATH){
 	string dir;
-	size_t pos=0, found;
-	string cleanPATH="";
+	size_t pos=0;
+	size_t found;
+	string clean;
 	while(true){
-		if((found=dirtyPATH.find(':',pos)) != string::npos){
-			dir=dirtyPATH.substr(pos,found-pos);
+		if((found=dirtyPATH.find(':', pos)) != string::npos){
+			dir = dirtyPATH.substr(pos,found-pos);
 		}else{
-			dir=dirtyPATH.substr(pos);
+			dir = dirtyPATH.substr(pos);
 		}
-		if(Util::dirExistsFollowingSymLink(jailPath+dir)){
-			if(cleanPATH.size()>0){
-				cleanPATH+=':';
+		if(Util::dirExistsFollowingSymLink(path+dir)){
+			if(clean.size()>0){
+				clean += ':';
 			}
-			cleanPATH+=dir;
+			clean += dir;
 		} else {
 			syslog(LOG_INFO, "Path removed: %s", dir.c_str());
 		}
-		if(found == string::npos) return cleanPATH;
+		if(found == string::npos) return clean;
 		pos=found+1;
 	}
 	return "";
 }
 
 void Configuration::checkConfigFile(string fileName, string men){
+	const int ga_mask = 077;
 	struct stat info;
-	if(lstat(fileName.c_str(),&info))
-		throw men+" not found "+fileName;
-	if(info.st_uid !=0 || info.st_gid !=0)
-		throw men+" not owned by root "+fileName;
-	if(info.st_mode & 077)
-		throw men+" with insecure permissions (must be 0600/0700)" +fileName;
+	if(lstat(fileName.c_str(), &info))
+		throw men + " not found " + fileName;
+	if(info.st_uid != 0 || info.st_gid != 0)
+		throw men + " not owned by root " + fileName;
+	if(info.st_mode & ga_mask)
+		throw men + " with insecure permissions (must be 0600/0700)" + fileName;
 }
 
 void Configuration::readConfigFile(){
 	ConfigData configDefault;
-	//Values used if parmeter not present in config file
-	configDefault["MAXTIME"]=Util::itos(1200);
-	configDefault["MAXFILESIZE"]=Util::itos(64*1024*1024);
-	configDefault["MAXMEMORY"]=Util::itos(2000*1024*1024);
-	configDefault["MAXPROCESSES"]=Util::itos(500);
-	configDefault["MIN_PRISONER_UGID"]="10000";
-	configDefault["MAX_PRISONER_UGID"]="20000";
-	configDefault["JAILPATH"]="/jail";
-	configDefault["CONTROLPATH"]="/var/vpl-jail-system";
-	configDefault["TASK_ONLY_FROM"]="";
-	configDefault["INTERFACE"]="";
-	configDefault["PORT"]="80";
-	configDefault["SECURE_PORT"]="443";
-	configDefault["URLPATH"]="/";
-	configDefault["ENVPATH"]=Util::getEnv("PATH");
-	configDefault["LOGLEVEL"]="0";
-	configDefault["FAIL2BAN"]="0";
-	configDefault["SSL_CIPHER_LIST"]="";
-	configDefault["SSL_CERT_FILE"]="/etc/vpl/cert.pem";
-	configDefault["SSL_KEY_FILE"]="/etc/vpl/key.pem";
-	ConfigData data=ConfigurationFile::readConfiguration(configPath,configDefault);
+	// Values used if parameter not present in configuration file
+	configDefault["MAXTIME"] = "1200";
+	configDefault["MAXFILESIZE"] = "64 Mb";
+	configDefault["MAXMEMORY"] = "2000 Mb";
+	configDefault["MAXPROCESSES"] = "500";
+	configDefault["MIN_PRISONER_UGID"] = "10000";
+	configDefault["MAX_PRISONER_UGID"] = "20000";
+	configDefault["JAILPATH"] = "/jail";
+	configDefault["CONTROLPATH"] = "/var/vpl-jail-system";
+	configDefault["TASK_ONLY_FROM"] = "";
+	configDefault["INTERFACE"] = "";
+	configDefault["PORT"] = "80";
+	configDefault["SECURE_PORT"] = "443";
+	configDefault["URLPATH"] = "/";
+	configDefault["ENVPATH"] = Util::getEnv("PATH");
+	configDefault["LOGLEVEL"] = "0";
+	configDefault["FAIL2BAN"] = "0";
+	configDefault["SSL_CIPHER_LIST"] = "";
+	configDefault["SSL_CERT_FILE"] = "/etc/vpl/cert.pem";
+	configDefault["SSL_KEY_FILE"] = "/etc/vpl/key.pem";
+	configDefault["USE_CGROUP"] = "false";
+	configDefault["REQUEST_MAX_SIZE"] = "128 Mb";
+	configDefault["RESULT_MAX_SIZE"] = "32 Kb";
+	ConfigData data=ConfigurationFile::readConfiguration(configPath, configDefault);
 	minPrisoner = atoi(data["MIN_PRISONER_UGID"].c_str());
 	if(minPrisoner < JAIL_MIN_PRISONER_UID)
-		throw "Incorrect MIN_PRISONER_UGID config value"
-		+data["MIN_PRISONER_UGID"];
-	maxPrisoner= atoi(data["MAX_PRISONER_UGID"].c_str());
+		throw "Incorrect MIN_PRISONER_UGID config value" + data["MIN_PRISONER_UGID"];
+	maxPrisoner = atoi(data["MAX_PRISONER_UGID"].c_str());
 	if(maxPrisoner > JAIL_MAX_PRISONER_UID)
-		throw "Incorrect MAX_PRISONER_UGID config value"
-		+data["MAX_PRISONER_UGID"];
+		throw "Incorrect MAX_PRISONER_UGID config value" + data["MAX_PRISONER_UGID"];
 	if(minPrisoner>maxPrisoner || minPrisoner<JAIL_MIN_PRISONER_UID || maxPrisoner>JAIL_MAX_PRISONER_UID)
 		throw "Incorrect config file, prisoner uid inconsistency";
 	jailPath = data["JAILPATH"];
 	if(!Util::correctPath(jailPath))
 		throw "Incorrect Jail"+jailPath;
-	jailPath=jailPath[0]=='/'?jailPath:"/"+jailPath; //Add absolute path
+	jailPath = jailPath[0] == '/' ? jailPath : "/" + jailPath; //Add absolute path
 	if(jailPath == "/")
 		throw "Jail path can NOT be set to /";
 	controlPath = data["CONTROLPATH"];
-	controlPath=controlPath[0]=='/'?controlPath:"/"+controlPath; //Add absolute path
+	controlPath = controlPath[0] == '/' ? controlPath : "/" + controlPath; //Add absolute path
 	if(!Util::correctPath(controlPath))
 		throw "Incorrect control path "+controlPath;
 	jailLimits.maxtime = atoi(data["MAXTIME"].c_str());
-	jailLimits.maxfilesize=atoi(data["MAXFILESIZE"].c_str());
-	jailLimits.maxmemory=atoi(data["MAXMEMORY"].c_str());
-	jailLimits.maxprocesses=atoi(data["MAXPROCESSES"].c_str());
-	string tof=data["TASK_ONLY_FROM"];
+	jailLimits.maxfilesize = Util::memSizeToBytesi(data["MAXFILESIZE"]);
+	jailLimits.maxmemory = Util::memSizeToBytesi(data["MAXMEMORY"]);
+	jailLimits.maxprocesses = atoi(data["MAXPROCESSES"].c_str());
+	string tof = data["TASK_ONLY_FROM"];
 	string dir;
 	size_t pos=0;
-	while((pos=tof.find(" ",pos)) != string::npos){
-		dir=tof.substr(0,pos);
+	while((pos=tof.find(" ", pos)) != string::npos){
+		dir=tof.substr(0, pos);
 		Util::trimAndRemoveQuotes(dir);
-		if(dir.length()>0)
+		if(dir.length() > 0)
 			taskOnlyFrom.push_back(dir);
-		tof.erase(0,pos);
+		tof.erase(0, pos);
 	}
-	dir=tof.substr(0,pos);
+	dir=tof.substr(0, pos);
 	Util::trimAndRemoveQuotes(dir);
-	if(dir.size()>0)
+	if(dir.size() > 0)
 		taskOnlyFrom.push_back(dir);
-	interface=data["INTERFACE"];
-	port=atoi(data["PORT"].c_str());
-	sport=atoi(data["SECURE_PORT"].c_str());
+	interface = data["INTERFACE"];
+	port = atoi(data["PORT"].c_str());
+	sport = atoi(data["SECURE_PORT"].c_str());
 	URLPath = data["URLPATH"];
 	cleanPATH = data["ENVPATH"];
 	logLevel = atoi(data["LOGLEVEL"].c_str());
@@ -116,13 +119,18 @@ void Configuration::readConfigFile(){
 	SSLCipherList = data["SSL_CIPHER_LIST"];
 	SSLCertFile = data["SSL_CERT_FILE"];
 	SSLKeyFile = data["SSL_KEY_FILE"];
+	useCgroup = Util::toUppercase(data["USE_CGROUP"]) == "TRUE";
+	requestMaxSize = Util::memSizeToBytesi(data["REQUEST_MAX_SIZE"]);
+	resultMaxSize = Util::memSizeToBytesi(data["RESULT_MAX_SIZE"]);
 }
 
-Configuration::Configuration(){
-	configPath="/etc/vpl/vpl-jail-system.conf";
-	checkConfigFile(configPath,"Config file");
+Configuration::Configuration() {
+	configPath = "/etc/vpl/vpl-jail-system.conf";
+	checkConfigFile(configPath, "Config file");
 	readConfigFile();
-//cleanPATH=generateCleanPATH(getJailPath(),cleanPATH);
 }
 
-
+Configuration::Configuration(string path) {
+	configPath = path;
+	readConfigFile();
+}
--- src/configuration.h
+++ src/configuration.h
@@ -12,34 +12,42 @@
 #include <vector>
 #include "util.h"
 using namespace std;
-class Configuration{
+
+class Configuration {
 private:
-	static Configuration* singlenton;
 	ExecutionLimits jailLimits;
 	string jailPath;   //Path to jail file system
 	string cleanPATH; //Path environment variable cleaned
 	string configPath; //Path to configuration file
 	string controlPath; //Path to the control directory
-	uid_t  minPrisoner, maxPrisoner; //uid prisoner selection limits
+	uid_t  minPrisoner; //Minimum value of range of uid prisoner
+	uid_t maxPrisoner; //Maximum value of range of uid prisoner
 	vector<string> taskOnlyFrom; //Moodle servers that can submit task
 	string interface; //Interface to serve default all
 	string URLPath; //URL path to accept tasks
-	int port, sport; //Server ports
+	int port; // Plain communications port number
+	int sport; // Ciphered communications port number
 	int logLevel; //Log level 0 to 8, 0 = no log, 8 += do not remove home dir.
 	int fail2ban; //Fail to ban configuration. Relation fail / success to ban IP
 	string SSLCipherList;
 	string SSLCertFile;
 	string SSLKeyFile;
+	bool useCgroup;
+	int requestMaxSize;
+	int resultMaxSize;
 	regex_t reg;
+protected:
+	static Configuration* singlenton;
 	void checkConfigFile(string fileName, string men);
 	Configuration();
+	Configuration(string path);
 public:
 	static Configuration* getConfiguration(){
-		if(singlenton == NULL) singlenton= new Configuration();
+		if(singlenton == NULL) singlenton = new Configuration();
 		return singlenton;
 	}
 	void readConfigFile();
-	static string generateCleanPATH(string jailPath, string dirtyPATH);
+	static string generateCleanPATH(string path, string dirtyPATH);
 	const ExecutionLimits & getLimits() const { return jailLimits;}
 	const string & getJailPath() const { return jailPath;}
 	const string &  getCleanPATH() const { return cleanPATH;}
@@ -56,6 +64,9 @@ public:
 	const string &  getSSLCipherList() const { return SSLCipherList;}
 	const string &  getSSLCertFile() const { return SSLCertFile;}
 	const string &  getSSLKeyFile() const { return SSLKeyFile;}
+	bool getUseCGroup() const { return useCgroup;};
+	int getRequestMaxSize() const { return requestMaxSize;};
+	int getResultMaxSize() const { return resultMaxSize;};
 };
 #endif
 
