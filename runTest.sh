0f1ce6e67731099bc7bb52cb93ec2284bfc4c2c1
jcrodriguez <jcrodriguez@dis.ulpgc.es>
2021-04-14 17:12:32 +0100

    Improves tests

--- runTest.sh
+++ runTest.sh
@@ -1,32 +1,128 @@
 #!/bin/bash
-aclocal
-autoheader
-autoconf
-automake
-./configure
-if [ "$1" == "" ] ; then
+# package:		Part of vpl-jail-system
+# copyright:    Copyright (C) 20XX Juan Carlos Rodriguez-del-Pino
+# license:      GNU/GPL, see LICENSE.txt or http://www.gnu.org/licenses/gpl-3.0.txt
+# Description:  Script to run tests for vpl-jail-system
+
+CHECK_MARK="\u2713";
+X_MARK="\u274C";
+function writeHeading {
+	echo -e "\e[33m RUNNING \e[0m \e[34m$1\e[0m"
+}
+function writeInfo {
+	echo -e $3 "\e[33m$1\e[0m$2"
+}
+function writeError {
+	echo -e "\e[31m$1\e[0m$2"
+}
+
+function writeCorrect {
+	echo -e "\e[32m$1\e[0m$2"
+}
+
+function write {
+	echo -e "$1"
+}
+
+export -f writeHeading
+export -f writeInfo
+export -f writeError
+export -f write
+
+function runTests() {
+	local test
+	local fmessages=messages.txt
+	local ferrors=errors.txt
+	local ntests="$#"
+	local n=1
+	writeInfo "" "$ntests tests found"
+	while [ "$1" != "" ] ; do
+		test=$1
+		writeInfo "Test $n" ": $test " -n
+		{
+			"$test"
+		} 1>$fmessages 2>$ferrors
+		if [ "$?" != "0" ] ; then
+			writeError "Errors found" " $X_MARK"
+			head -n 10 $ferrors
+			head -n 10 $fmessages
+			rm $fmessages 2> /dev/null
+			rm $ferrors 2> /dev/null
+			exit 1
+		else
+			writeCorrect "$CHECK_MARK"
+		fi
+		((n=n+1))
+		shift
+	done
+	rm $fmessages 2> /dev/null
+	rm $ferrors 2> /dev/null
+	write
+	writeCorrect "All tests passed $CHECK_MARK"
+}
+
+function Autotools_execution() {
+	aclocal
+	autoheader
+	autoconf
+	automake
+	./configure
+}
+
+function Packaging_for_distribution() {
 	make distcheck
-	if [ "$?" != "0" ] ; then
-		echo "Error: make distcheck fails."
-		exit 1
-	fi
-fi
-cd tests
-make program-test
-if test -f program-test ; then
-	rm -R cgroup.test 2> /dev/null
-	cp -a cgroup cgroup.test
-	./program-test
-	if [ "$?" != "0" ] ; then
-		tail /var/log/syslog
-		exit 1
-	else
+}
+
+function Unit_tests() {
+	local result
+	cd tests
+	make program-test
+	if test -f program-test ; then
+		rm -R cgroup.test 2> /dev/null
+		cp -a cgroup cgroup.test
+		./program-test
+		result=$?
 		rm -R cgroup.test 2> /dev/null
 		rm program-test
-		echo "All tests passed."
-		exit 0
+		cd ..
+		if [ "$result" != "0" ] ; then
+			tail /var/log/syslog
+			return 1
+		else
+			return 0
+		fi
 	fi
-else 
-	echo "Error: program-test not found."
-	exit 2
+	cd ..
+	return 1
+}
+
+function WebSocket_tests() {
+	local result
+	local running
+	cd tests
+	make websocket-echo-test
+	if test -f websocket-echo-test ; then
+		./websocket-echo-test &
+		running=$!
+		python3 ./webSocketClient.py
+		result=$?
+		kill $running
+		cd ..
+		if [ "$result" != "0" ] ; then
+			tail /var/log/syslog
+			return 1
+		else
+			return 0
+		fi
+	fi
+	cd ..
+	return 1
+}
+
+writeHeading "Tests of the vpl-jail-system"
+if [ "$1" != "" ] ; then
+	runTests $1
+else
+	runTests Autotools_execution Packaging_for_distribution Unit_tests WebSocket_tests
 fi
+
--- tests/webSocketClient.py
+++ tests/webSocketClient.py
@@ -1,24 +1,29 @@
 import time
 import websocket
+def checkEcho(conn, value):
+    print ('Sending ', value)
+    conn.send(value)
+    time.sleep(0.1)
+    result = str(conn.recv())
+    print ('Received', result)
+    if result != value:
+        print ('Error incorrect response')
+        return False
+    else:
+        return True
+
 conn = websocket.create_connection('ws://localhost:8080')
 print (str(conn.recv()))
-time.sleep(0.1)
-conn.send('hola')
-time.sleep(0.1)
-print (str(conn.recv()))
+checkEcho(conn, 'Hello!')
+checkEcho(conn, 'This is other text with no ASCII chars ññññáéÇ')
 conn.send('close')
 time.sleep(0.1)
 print (str(conn.recv()))
 conn.close()
 conn = websocket.create_connection('ws://localhost:8080')
 print (str(conn.recv()))
-time.sleep(0.1)
-conn.send('Adios')
-time.sleep(0.1)
-print (str(conn.recv()))
-conn.send(b'texto en binario')
-time.sleep(0.1)
-print (conn.recv())
+checkEcho(conn, 'Second try!')
+checkEcho(conn, b'Text as bynary array')
 conn.send('end')
 time.sleep(0.1)
 print (str(conn.recv()))
