9048e5f295330d495d9dd80bb6209b18e2e4e356
Juan Carlos Rodríguez del Pino <jc.rodriguezdelpino@ulpgc.es>
2023-11-24 02:04:27 +0000

    Using Logger and rename mode to foreground

--- src/main.cpp
+++ src/main.cpp
@@ -13,51 +13,54 @@ using namespace std;
 
 #ifndef TEST
 /**
- * main accept command line "online" to go nondemonize run.
+ * main accept command line "foreground" to go nondemonize run.
  */
 int main(int const argc, const char ** const argv, char * const * const env) {
-	bool online = false;
+	bool foreground = false;
 	for (int i = 1; i < argc; i++) {
-		if (string(argv[i]) == "online") {
-			online = true;
+		if (string(argv[i]) == "foreground") {
+			foreground = true;
+			cout << "Service execution foreground (nondemonize)" << endl;
+			break;
 		}
 	}
-	Logger::setLogLevel(LOG_ERR, online);
+	Logger::setLogLevel(LOG_ERR, foreground);
 	Configuration *conf = Configuration::getConfiguration();
-	Logger::setLogLevel(conf->getLogLevel(), online);
+	Logger::setLogLevel(conf->getLogLevel(), foreground);
 	if ( conf->getLogLevel() >= LOG_INFO) {
 		conf->readConfigFile(); // Reread configuration file to show values in log
 	}
 	int exitStatus=static_cast<int>(internalError);
 	try{
 		Daemon* runner = Daemon::getRunner();
-		if (online) {
-			runner->online();
+		if (foreground) {
+			runner->foreground();
 		} else {
 			runner->demonize();
 		}
+		Logger::log(LOG_INFO, "Server started");
 		runner->loop();
 		exitStatus = EXIT_SUCCESS;
 	}
 	catch(HttpException &exception) {
-		Logger::log(LOG_ERR, "%s", exception.getLog().c_str());
+		Logger::log(LOG_EMERG, "%s", exception.getLog().c_str());
 		exitStatus=static_cast<int>(httpError);
 	}
 	catch(const string &me) {
 		exitStatus = EXIT_FAILURE;
-		Logger::log(LOG_ERR, "%s", me.c_str());
+		Logger::log(LOG_EMERG, "%s", me.c_str());
 	}
 	catch(const char * const me) {
 		exitStatus = EXIT_FAILURE;
-		Logger::log(LOG_ERR, "%s",me);
+		Logger::log(LOG_EMERG, "%s",me);
 	}
 	catch(std::exception &e) {
 		exitStatus = EXIT_FAILURE;
-		Logger::log(LOG_ERR, "Unexpected exception: %s %s:%d", e.what(), __FILE__, __LINE__);
+		Logger::log(LOG_EMERG, "Unexpected exception: %s %s:%d", e.what(), __FILE__, __LINE__);
 	}
 	catch(...){
 		exitStatus = EXIT_FAILURE;
-		Logger::log(LOG_ERR, "Unexpected exception %s:%d", __FILE__, __LINE__);
+		Logger::log(LOG_EMERG, "Unexpected exception %s:%d", __FILE__, __LINE__);
 	}
 	exit(exitStatus);
 }
