4e5e8b3e9976c92a3296c9982ead51a1e9a22e54
Juan Carlos Rodríguez del Pino <jc.rodriguezdelpino@ulpgc.es>
2023-11-28 22:43:29 +0000

    Improves launch of different VNC versions
    Launchs on Ububtu, Debian, Fedora, etc.
    Not full functional yet.

--- vpl_vnc_launcher.sh
+++ vpl_vnc_launcher.sh
@@ -1,76 +1,153 @@
 #!/bin/bash
+mkdir -p $HOME/.vnc
 {
-	. vpl_environment.sh
-	for NEWLANG in $VPL_LANG en_US.UTF-8 C.utf8 POSIX C
-	do
-		export LC_ALL=$NEWLANG 2> .vpl_set_locale_error
-		if [ -s .vpl_set_locale_error ] ; then
-			rm .vpl_set_locale_error
-			continue
-		else
-			break
-		fi
-	done
-	rm .vpl_set_locale_error
-	mkdir .vnc
-	VNCACCELDIR=/etc/vncaccel
-	if [ -d "$VNCACCELDIR" ] ; then
-		cp -a $VNCACCELDIR/.??* $HOME
-	fi
-	#try vcnpasswd: 1-tightvnc 2:tigervnc
-	printf "$VPL_VNCPASSWD\n$VPL_VNCPASSWD\n" | vncpasswd -f >$HOME/.vnc/passwd
-	if [ ! -s $HOME/.vnc/passwd ] ; then
-		printf "$VPL_VNCPASSWD\n$VPL_VNCPASSWD\n" | vncpasswd
-	fi
-	chmod 0600 $HOME/.vnc/passwd
-	cat >$HOME/.vnc/xstartup <<"END_OF_FILE"
+    . vpl_environment.sh
+    for NEWLANG in $VPL_LANG en_US.UTF-8 C.utf8 POSIX C
+    do
+        [ "$(export LC_ALL=$NEWLANG 2>&1)" != "" ] && continue
+        break
+    done
+    echo "LC_ALL => $NEWLANG"
+    VNCACCELDIR=/etc/vncaccel
+    [ -d "$VNCACCELDIR" ] && cp -a $VNCACCELDIR/.??* $HOME
+    VNCPASSWDFILE=$HOME/.vnc/passwd
+    VNCPASSWDSET="$VPL_VNCPASSWD\n$VPL_VNCPASSWD\nn\n"
+    printf "$VNCPASSWDSET" | vncpasswd -f >$VNCPASSWDFILE
+    [ ! -s $VNCPASSWDFILE ] && 	printf "$VNCPASSWDSET" | vncpasswd
+    chmod 0600 $VNCPASSWDFILE
+
+    [ "$VPL_XGEOMETRY" == "" ] && VPL_XGEOMETRY="800x600"
+    echo "VPL_XGEOMETRY => $VPL_XGEOMETRY"
+    if [ "$(command -v nc)" != "" ] ; then
+        while true; do
+            export VNCPORT=$((5900+$RANDOM%95))
+            [ "$(nc -z 127.0.0.1 $VNCPORT ; echo $?)" == "1" ] && break
+            ((limit++)) && ((limit==100)) && break
+        done
+    else
+        while true; do
+            export VNCPORT=$((5900+$RANDOM%95))
+            lsof -i :$VNCPORT &>/dev/null
+            [ "$?" != "0" ] && break
+            ((limit++)) && ((limit==100)) && break
+        done
+    fi
+    echo "VNCPORT => $VNCPORT"
+    ((NDIS=$VNCPORT-5900))
+    export NDIS
+    COOKIE=$(mcookie)
+    [ "$?" != "0" ] && COOKIE="$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM"
+    touch $HOME/.Xauthority
+    printf "add :$NDIS . ${COOKIE:1:10}\nexit\n" | xauth
+    export XSTARTUPFILE=$HOME/.vnc/xstartup
+    cat >$XSTARTUPFILE <<"END_OF_SCRIPT"
 #!/bin/bash
+export DISPLAY=:$NDIS
 export TERM=xterm
 unset SESSION_MANAGER
-xrdb
-xsetroot -solid MidnightBlue 
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+FONTPATHS=( '/usr/share/X11/fonts' '/usr/share/fonts/X11/' '/usr/lib/X11/fonts'
+            '/usr/X11/lib/X11/fonts' '/usr/X11R6/lib/X11/fonts' '/usr/X11/lib/X11/fonts' )
+FTYPES=( 'misc' '75dpi' '100dpi' 'Speedo' 'Type1' )
+for FONTPATH in "${FONTPATHS[@]}" ; do
+    if [ -d $FONTPATH ] ; then
+        for FTYPE in "${FTYPES[@]}" ; do
+            FONT=${FONTPATH}/${FTYPE}
+            if [ -f "${FONT}/fonts.dir" ] ; then
+                [ -z "${FONTS}" ] && FONTS="${FONT}"
+                [ -n "${FONTS}" ] && FONTS="${FONTS},${FONT}"
+            fi
+        done
+    fi
+done
+
+[ "$1" == "xinitrc" ] && [ -x /etc/X11/xinit/xinitrc ] && /etc/X11/xinit/xinitrc
+[ -x "$(command -v xset)" ] && xset fp= $FONTS
+[ -x "$(command -v xsetroot)" ] && xsetroot -solid MidnightBlue 
 #activate clipboard
 [ -x "$(command -v vncconfig)" ] && vncconfig -iconic 2>/dev/null &
 #start window manager
 if [ -x "$(command -v icewm)" ] ; then
-	mkdir .icewm
-	echo "Theme=SilverXP/default.theme" > .icewm/theme
-	icewm &
+    mkdir -p .icewm
+    echo "Theme=SilverXP/default.theme" > .icewm/theme
+    icewm &
 elif [ -x "$(command -v fluxbox)" ] ; then
-	fluxbox &
+    fluxbox &
 elif [ -x "$(command -v openbox)" ] ; then
-	openbox &
+    openbox &
 elif [ -x "$(command -v metacity)" ] ; then
-	metacity &
+    metacity &
 else
-	xmessage "Window Manager not found"
+    [ -x "$(command -v xmessage)" ] && xmessage "Window Manager not found"
 fi
-#set execution mode
-chmod +x $HOME/vpl_wexecution
+# Set execution mode
+chmod +x $HOME/vpl_wexecution &> .std_output
 $HOME/vpl_wexecution &> .std_output
 if [ -s .std_output ] ; then
-	if [ "$(command -v xterm)" != "" ] ; then
-		xterm -T "std output" -bg white -fg red -e less .std_output
-	else
-		if [ "$(command -v x-terminal-emulator)" != "" ] ; then
-			x-terminal-emulator -e less .std_output
-		fi
-	fi
+    if [ -x "$(command -v xterm)" ] ; then
+        xterm -T "std output" -bg white -fg red -e more .std_output
+    else
+        if [ -x "$(command -v x-terminal-emulator)" ] ; then
+            x-terminal-emulator -e more .std_output
+        fi
+    fi
 fi
-VNCDISPLAY=$(ls $HOME/.vnc/*.log | sed -e "s/[^:]*://" -e "s/\.log$//")
-vncserver -kill :$VNCDISPLAY
-END_OF_FILE
-	if [ "$VPL_XGEOMETRY" == "" ] ; then
-		VPL_XGEOMETRY="800x600"
-	fi
-	chmod 0755 $HOME/.vnc/xstartup
-	while true; do
-		export VNCPORT=$((6000+$RANDOM%25000))
-		lsof -i :$VNCPORT
-		[ "$?" != "0" ] && break
-	done
-	export DISPLAY=:$VNCPORT.0
-	nohup vncserver :$VNCPORT -rfbport $VNCPORT -localhost -geometry $VPL_XGEOMETRY -nevershared -name vpl
-} &>/dev/null
-echo $VNCPORT
 
+END_OF_SCRIPT
+
+    chmod 0755 $XSTARTUPFILE
+} &>$HOME/.vnc/starting.log
+echo $VNCPORT
+{
+
+    if [ -x "$(command -v tightvncserver)" ] ; then
+        echo "Using tightvncserver"
+        nohup tightvncserver \
+            -rfbport $VNCPORT \
+            -geometry $VPL_XGEOMETRY \
+            -localhost \
+            -nevershared \
+            -name vpl \
+            :$NDIS > $HOME/.vnc/vncserver.log
+    elif [ -x "$(command -v Xvnc)" ] ; then
+        if [ "$(Xvnc -version | grep -q TigerVNC ; echo $?)" = "0" ] ; then
+            echo "Using TigerVNC with Xvnc"
+            $XSTARTUPFILE xinitrc &
+            {
+                echo "rfbport=$VNCPORT"
+                echo "geometry $VPL_XGEOMETRY"
+                echo "localhost"
+                echo "nevershared"
+            } > $HOME/.vnc/config
+            nohup Xvnc \
+                -rfbport=$VNCPORT \
+                -nevershared \
+                -localhost \
+                -SecurityTypes=VncAuth \
+                -PasswordFile=$VNCPASSWDFILE \
+                -geometry $VPL_XGEOMETRY \
+                -desktop vpl$NDIS \
+                :$NDIS > $HOME/.vnc/vncserver.log
+        else
+            echo "Using Tightvnc with Xvnc"
+            $XSTARTUPFILE xinitrc &
+            echo "Xvnc"
+            {
+                echo "rfbport=$VNCPORT"
+                echo "geometry $VPL_XGEOMETRY"
+                echo "localhost"
+                echo "nevershared"
+            } > $HOME/.vnc/config
+            nohup Xvnc \
+                -rfbport=$VNCPORT \
+                -nevershared \
+                -localhost \
+                -SecurityTypes=VncAuth \
+                -geometry $VPL_XGEOMETRY \
+                -name vpl$NDIS \
+                :$NDIS > $HOME/.vnc/vncserver.log
+        fi
+    fi
+    sleep 10d
+} &>>$HOME/.vnc/starting.log
