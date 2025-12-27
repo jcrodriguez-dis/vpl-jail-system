0867fde5d0fa0b91fb98c31948558d890002ea32
Juan Carlos Rodriguez-del-Pino <jcrodriguez@dis.ulpgc.es>
2025-12-27 17:25:07 +0000

    Bump version to 5.0.0 in configuration files

--- config.h
+++ config.h
@@ -152,7 +152,7 @@
 #define PACKAGE_NAME "vpl jail system"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "vpl jail system 4.0.4"
+#define PACKAGE_STRING "vpl jail system 5.0.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "vpl-jail-system"
@@ -161,13 +161,13 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "4.0.4"
+#define PACKAGE_VERSION "5.0.0"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "4.0.4"
+#define VERSION "5.0.0"
 
 /* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
    <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
